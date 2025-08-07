3d771193d1f72c56a7337c87913b2946c335eda3
Lukas Pertl <ljfp2@cam.ac.uk>
2025-08-07 11:31:01 +0100

    Experiments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ExperimentalPipeline1 import main
 
 # Specific rows correspond to configurations defined in the CSV files (See ExperimentList folder).
 if __name__ == '__main__':
-    specific_rows = [201,202,203,204,205]
+    specific_rows = [210,215]
     Mode = "simple"
     main(specific_rows, Mode)
 
